ca480e386d1d2f74837c0ca4efa039566218c1eb
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-10-28 15:49:38 +0700

    Koneksi Database

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+db.sequelize.sync().then(() => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+
+ })
+ .catch((err) => {
+    console.log(err);
+ })
 
